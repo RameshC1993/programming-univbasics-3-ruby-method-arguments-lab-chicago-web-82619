ae2249e0c7947739212a307f0dcc9ae8515f293a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-17 03:13:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,10 +5,12 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
+introduction_with_language("Dan", "Ember.js")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language_optional("Edwin")
 
 
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
